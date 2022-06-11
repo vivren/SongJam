57e1d5b2473aea39c0beaf9d40153c35ebdd5982
vivren <rren.vivian@gmail.com>
2022-06-11 19:16:50 -0400

    added basic ui to room.html

--- classes.py
+++ classes.py
@@ -1,23 +1,26 @@
 from collections import deque
+import json
 
 class Song:
     def __init__(self, title, id):
         self.title = title
         self.id = id
-        self.link = f"https://www.youtube.com/embed/{id}?autoplay=1"
 
     def getTitle(self):
         return self.title
 
-    def getLink(self):
-        return self.link
-
     def getID(self):
         return self.id
+
+    def __str__(self):
+        return self.getTitle() + "," + self.getID()
 
 class Playlist:
     def __init__(self):
         self.playlist = deque()
+
+    def isEmpty(self):
+        return len(self.playlist) == 0
 
     def addSong(self, song):
         self.playlist.append(song)
@@ -38,6 +41,21 @@ class Playlist:
     def getPlaylist(self):
         returnVal = []
         for song in self.playlist:
-            returnVal.append(song.getTitle())
+            returnVal.append(str(song))
         return returnVal
+
+    # def convertJson(self):
+    #     jsonString = '{"songs": ['
+    #     for song in self.playlist:
+    #         jsonString += "{"
+    #         jsonString += f'"name": "{song.getTitle()}", "id": "{song.getID()}"'
+    #         if song == self.getLastSong():
+    #             jsonString += "}"
+    #         else:
+    #             jsonString += "},"
+    #     jsonString += "]}"
+    #     return jsonString
+
+
+
 
