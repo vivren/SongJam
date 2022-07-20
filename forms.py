04b94d60a7846fff524ddf4e0bae8bffbcb4f956
vivren <rren.vivian@gmail.com>
2022-07-20 00:00:10 -0400

    removing old files

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    name = StringField(validators=[DataRequired()], render_kw={'placeholder': 'Nickname', 'class': 'form-control', 'style': 'width: 75%; margin: auto;'})
-    room = StringField(validators=[DataRequired()], render_kw={'placeholder': 'Room Name', 'class': 'form-control', 'style': 'width: 75%; margin: auto;'})
-    submit = SubmitField('Join', render_kw={'class': 'btn btn-light'})
