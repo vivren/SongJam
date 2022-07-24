2a44df8d4c44da8ee9ea6d963031ddccbb61cecc
vivren <rren.vivian@gmail.com>
2022-07-24 00:46:59 -0400

    added roomAction forms

--- app/roomAction/forms.py
+++ app/roomAction/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField
+from wtforms.fields import StringField, RadioField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
+class CreateForm(FlaskForm):
     name = StringField(validators=[DataRequired()], render_kw={'placeholder': 'Nickname', 'class': 'form-control', 'style': 'width: 75%; margin: auto;'})
-    room = StringField(validators=[DataRequired()], render_kw={'placeholder': 'Room Name', 'class': 'form-control', 'style': 'width: 75%; margin: auto;'})
+    roomName = StringField(validators=[DataRequired()], render_kw={'placeholder': 'Room Name', 'class': 'form-control', 'style': 'width: 75%; margin: auto;'})
+    roomType = RadioField('Room Type', choices=['Public', 'Private'], validators=[DataRequired()])
+    submit = SubmitField('Create', render_kw={'class': 'btn btn-light'})
+
+class JoinForm(FlaskForm):
+    name = StringField(validators=[DataRequired()], render_kw={'placeholder': 'Nickname', 'class': 'form-control', 'style': 'width: 75%; margin: auto;'})
+    roomName = StringField(validators=[DataRequired()], render_kw={'placeholder': 'Room Name', 'class': 'form-control', 'style': 'width: 75%; margin: auto;'})
     submit = SubmitField('Join', render_kw={'class': 'btn btn-light'})
