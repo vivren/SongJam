1010eedeea0bcd27670602af91bb73ad8c7946a7
vivren <rren.vivian@gmail.com>
2022-07-26 01:25:16 -0400

    added password and roomId validator to join room

--- app/roomAction/classes.py
+++ app/roomAction/classes.py
@@ -2,27 +2,24 @@ import redis
 
 class Rooms:
     def __init__(self):
-        self.rooms = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
-        self.playlist.flushdb()
+        self.rooms = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True, db=1)
+        self.rooms.flushdb()
 
-    def searchRooms(self):
-        # Return rooms matching the search criteria
-
-
-class Room:
-    def __init__(self, *args):
-        self.name = name
-        self.type = type
-        self.password = None
+    def addRoom(self, *args):
+        if len(args) == 3:
+            args.append(None)
+        self.rooms.hmset(args[0], {"id": args[1], "type": args[2], "password": args[3]})
 
     def validatePassword(self):
         pass
 
-    def getRoomType(self):
+    def getRoomId(self):
+        return self.id
 
-    def addRoom(self):
-        pass
+    def getRoomType(self):
+        return self.type
 
     def removeRoom(self):
         pass
+
 
--- app/roomAction/roomAction.py
+++ app/roomAction/roomAction.py
@@ -1,38 +1,38 @@
 from flask import Flask, Blueprint, session, redirect, url_for, render_template, request
 from . import roomActionBP
 from .forms import CreateForm, JoinForm
+from .classes import Rooms
 import shortuuid
 
-rooms = 0
+rooms = Rooms()
 
 @roomActionBP.route('/createRoom', methods=['GET', 'POST'])
 def createRoom():
     form = CreateForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
-        roomId = shortuuid.uuid()
-        session['room'] = [form.roomName.data, roomId, form.roomType.data, form.roomPassword.data]
+        session['room'] = form.roomName.data
+        roomId = shortuuid.ShortUUID().random(length=5)
+        session['roomId'] = roomId
+        rooms.addRoom(Room(roomId, form.name.data, form.roomType.data, form.roomPassword.data))
         return redirect(url_for('room.room', roomId=roomId))
     elif request.method == 'GET':
-        print(form.errors)
         form.name.data = session.get('name', '')
-        form.roomName.data = session.get('room', '')[0]
-        form.roomType.data = session.get('room', '')[2]
-        form.roomPassword.data = session.get('room', '')[3]
-    return render_template('roomAction/createRoom.html', form=form)
+        form.roomName.data = session.get('room', '')
+        return render_template('roomAction/createRoom.html', form=form)
 
 
-@roomActionBP.route('/joinRoom')
+@roomActionBP.route('/joinRoom', methods=['GET', 'POST'])
 def joinRoom():
     form = JoinForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
-        session['room'] = form.room.data
-        return redirect(url_for('room.room'))
+        session['roomId'] = form.roomId.data
+        return redirect(url_for('room.room', roomId=roomId))
     elif request.method == 'GET':
         form.name.data = session.get('name', '')
-        form.roomName.data = session.get('roomName', '')
-    return render_template('roomAction/joinRoom.html', form=form)
+        form.roomId.data = session.get('roomId', '')
+        return render_template('roomAction/joinRoom.html', form=form)
 
 @roomActionBP.route('/browseRoom')
 def browseRoom():
